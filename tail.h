a0fffddb6e0b526cbf10c337ce08ef4a434e9f6d
Zainan Ali <44395608+zainanz@users.noreply.github.com>
2025-02-10 19:15:35 +0000

    Add files via upload

---
+++ tail.h
@@ -0,0 +1,11 @@
+#ifndef TAIL_H
+# define TAIL_H
+
+#include <unistd.h>
+void	ft_putstr(char *);
+int		ft_strcmp(char *, char *);
+int		ft_strlen(char *);
+int		ft_atoi_error(char *);
+int		ft_atoi(char *);
+
+#endif
